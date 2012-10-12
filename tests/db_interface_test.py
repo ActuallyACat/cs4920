abad9f16fd05153a4be0d4f7e63c8745bbd8840c
qq2233 <jibin.jing@gmail.com>
2012-10-13 00:00:44 +1200

    Update tests/db_interface_test.py

--- tests/db_interface_test.py
+++ tests/db_interface_test.py
@@ -10,7 +10,7 @@ from sample_entries_1 import *
 class MyTest(unittest.TestCase):
 
     def setUp(self):
-        '''For each test case, setup a database.'''
+    """For each test case, setup a database."""
         unittest.TestCase.setUp(self)
         try:
             os.remove('test.db')
@@ -20,20 +20,24 @@ class MyTest(unittest.TestCase):
         self.dbi.load_schema()
 
     def testCreateLibrary(self):
+    """Test if a new Library can be created"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         self.assertEqual(lib_id, 1)
 
     def testGetLibrary(self):
+    """Test if a library can be retrieved"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         lib_id = self.dbi.get_or_create_library_id('Test2', 1)
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         self.assertEqual(lib_id, 1)
 
     def testCreateMorpheme(self):
+    """Test if a Morpheme can be created"""
         morph_id = self.dbi.get_or_create_morpheme_id(u'は', 1)
         self.assertEqual(morph_id, 1)
 
     def testGetMorpheme(self):
+    """Test if morpheme can be retrieved"""
         morph_id = self.dbi.get_or_create_morpheme_id(u'は', 1)
         morph_id = self.dbi.get_or_create_morpheme_id(u'を', 1)
         morph_id = self.dbi.get_or_create_morpheme_id(u'は', 2)
@@ -43,12 +47,14 @@ class MyTest(unittest.TestCase):
         self.assertEqual(morph_id, 3)
 
     def testCreateEntry(self):
+    """Test if a new entry can be created"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         entry_id = self.dbi.create_entry_id(lib_id, 1, kana_id)
         self.assertEqual(entry_id, 1)
 
     def testCreateEntryHasKanji(self):
+    """Test if a new entry with kanji can be created"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         kanji_id = self.dbi.get_or_create_morpheme_id(u'愛', 14)
@@ -58,6 +64,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(entryhaskanji_id, 1)
 
     def testCreateMeaning(self):
+    """Test if a meaning can be created"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         kanji_id = self.dbi.get_or_create_morpheme_id(u'愛', 14)
@@ -66,11 +73,13 @@ class MyTest(unittest.TestCase):
         self.assertEqual(meaning_id, 1)
 
     def testCreateUsageExample(self):
+    """Test if a usage example can be created"""
         ue_id = self.dbi.create_usage_example_id(
                 'expression', 'meaning', 'reading', True)
         self.assertEqual(ue_id, 1)
 
     def testCreateMeaningHasUE(self):
+    """Test if a meaning can be created that has an associated usage example"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         kanji_id = self.dbi.get_or_create_morpheme_id(u'愛', 14)
@@ -82,6 +91,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(h_id, 1)
 
     def testCreateUEPartOfLibrary(self):
+    """Test if UE can be created as a part of a library"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         kanji_id = self.dbi.get_or_create_morpheme_id(u'愛', 14)
@@ -94,6 +104,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(l_id, 1)
 
     def testCreateUEConsistsOf(self):
+    """Test UE can be created with a library id, kana id, kanji id, entry id, meaning id and UE id"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         kana_id = self.dbi.get_or_create_morpheme_id(u'は', 15)
         kanji_id = self.dbi.get_or_create_morpheme_id(u'愛', 14)
@@ -106,6 +117,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(c_id, 1)
 
     def testImportEntry(self):
+    """Test if entries can be imported"""
         lib_id = self.dbi.get_or_create_library_id('Test', 1)
         parser = JapaneseParser()
         for entry in SAMPLE_ENTRIES_1:

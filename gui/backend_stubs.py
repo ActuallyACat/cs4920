99898b7efaf03bbd0867db853c5c3216ec606683
mathew <mathewchong7@gmail.com>
2012-10-12 15:40:14 +1100

    backend stub for list retrieval

--- gui/backend_stubs.py
+++ gui/backend_stubs.py
@@ -31,4 +31,6 @@ def lookup_mode_search(string):
 def get_user_ue_lists():
     list_of_ue_lists = list()
     ue_list = UsageExampleList('List1')
+    ue_list.set_ue_list([])
+    list_of_ue_lists.append(ue_list)
     #ue_list.add_usage_example
